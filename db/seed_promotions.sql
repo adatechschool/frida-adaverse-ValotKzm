d4e48469eeb099ce785f740c957ab4534b0369b2
Yannick Souza <darkvalot@gmail.com>
2025-11-25 14:15:44 +0100

    Created default seeds

---
+++ db/seed_promotions.sql
@@ -0,0 +1,5 @@
+INSERT INTO promotions (name, start_date) VALUES
+('Fatoumata Kébé', '2024-09-01'),
+('Frances Spence', '2025-01-01'),
+('Frida Kahlo', '2025-05-01'),
+('Grace Hopper', '2025-09-01');
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,5 @@
 import 'dotenv/config';
-import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
-dotenv.config({
-  path: ".env",
-})
 
 export default defineConfig({
   out: './db/migrations',
