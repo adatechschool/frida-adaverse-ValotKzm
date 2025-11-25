b910a8e213ab0f7e32d5d25202a48451cfb53637
Yannick Souza <darkvalot@gmail.com>
2025-11-25 11:01:11 +0100

    Created Schemas : projects_ada, promotions, student_projects

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,10 @@
 import 'dotenv/config';
+import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+dotenv.config({
+  path: ".env",
+})
+
 export default defineConfig({
   out: './db/migrations',
   dialect: 'postgresql',
